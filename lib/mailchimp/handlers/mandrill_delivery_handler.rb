90c54fa49c3b1ee674419e079ab75cc0a121a212
Dave Worth <dave@highgroove.com>
2013-01-02 09:38:11 -0500

    re-implementation of @highgroovedan's API update patch
    * The patch in [this](https://github.com/mailchimp/mailchimp-gem/pull/7) pull
      request provides important functionality but the implementation exposed
      internal implementation for the sake of testing.  Instead I've nested it away
      as a private method and just called it directly using `#send`.  While testing
      private methods is something of a no-no, I prefer this to exposing an
      interface where one should noe be.

--- lib/mailchimp/handlers/mandrill_delivery_handler.rb
+++ lib/mailchimp/handlers/mandrill_delivery_handler.rb
@@ -1,36 +1,16 @@
 module Mailchimp
   class MandrillDeliveryHandler
-    attr_accessor :settings, :message_payload
+    attr_accessor :settings
 
     def initialize(options = {})
       self.settings = {:track_opens => true, :track_clicks => true, :from_name => 'Mandrill Email Delivery Handler'}.merge(options)
     end
 
     def deliver!(message)
-      
-      self.message_payload = {
-        :track_opens => settings[:track_opens],
-        :track_clicks => settings[:track_clicks],
-        :message => {
-          :subject => message.subject,
-          :from_name => message.header['from-name'].blank? ? settings[:from_name] : message.header['from-name'],
-          :from_email => message.from.first,
-          :to => message.to.map {|email| { :email => email, :name => email } },
-          :headers => {'Reply-To' => message.reply_to.nil? ? nil : message.reply_to }
-        }
-      }
-
-      [:html, :text].each do |format|
-        content = get_content_for(message, format)
-        self.message_payload[:message][format] = content if content
-      end
-
-      self.message_payload[:tags] = settings[:tags] if settings[:tags]
-      
       api_key = message.header['api-key'].blank? ? settings[:api_key] : message.header['api-key']
       
-      self.settings[:return_response] = Mailchimp::Mandrill.new(api_key).messages_send(self.message_payload)
-      
+      message_payload = get_message_payload(message)
+      self.settings[:return_response] = Mailchimp::Mandrill.new(api_key).messages_send(message_payload)
     end
     
     private
@@ -45,7 +25,27 @@ module Mailchimp
       content ||= message.body if message.mime_type == mime_types[format]
       content
     end
-    
+
+    def get_message_payload(message)
+      message_payload = {
+        :track_opens => settings[:track_opens],
+        :track_clicks => settings[:track_clicks],
+        :message => {
+          :subject => message.subject,
+          :from_name => message.header['from-name'].blank? ? settings[:from_name] : message.header['from-name'],
+          :from_email => message.from.first,
+          :to => message.to.map {|email| { :email => email, :name => email } },
+          :headers => {'Reply-To' => message.reply_to.nil? ? nil : message.reply_to }
+        }
+      }
+      [:html, :text].each do |format|
+        content = get_content_for(message, format)
+        message_payload[:message][format] = content if content
+      end
+
+      message_payload[:tags] = settings[:tags] if settings[:tags]
+      message_payload
+    end
   end
 end
 
--- test/unit/handlers/mandrill_deliver_handler_test.rb
+++ test/unit/handlers/mandrill_deliver_handler_test.rb
@@ -19,26 +19,27 @@ class MandrillDeliveryHandlerTest < Test::Unit::TestCase
       response = @mandrill_delivery_handler.deliver!(message)
       assert_equal @fake_mandrill_api_response, response 
     end
-    
+
     should "store the api response in the settings[:return_response] hash key" do
       message = mock_mail_message
       response = @mandrill_delivery_handler.deliver!(message)
       assert_equal @fake_mandrill_api_response, @mandrill_delivery_handler.settings[:return_response]
     end
-    
+
     context "for the mandrill api payload" do
       should "build a valid Mandrill API payload" do
         message = mock_mail_message
         @mandrill_delivery_handler.deliver!(message)
-        
-        assert_equal true, @mandrill_delivery_handler.message_payload[:track_opens]
-        assert_equal true, @mandrill_delivery_handler.message_payload[:track_clicks]
-        assert_equal 'Subject', @mandrill_delivery_handler.message_payload[:message][:subject]
-        assert_equal 'foo@bar.com', @mandrill_delivery_handler.message_payload[:message][:from_email]
-        assert_equal 'Mandrill Email Delivery Handler', @mandrill_delivery_handler.message_payload[:message][:from_name]
-        assert_equal 'foo@bar.com has moved use awesome@awesomesauce.com now', @mandrill_delivery_handler.message_payload[:message][:html]
-        assert_equal [{email: 'foo@bar.com', name: 'foo@bar.com'}], @mandrill_delivery_handler.message_payload[:message][:to]
-        assert_equal({'Reply-To' => 'replyto@example.com'}, @mandrill_delivery_handler.message_payload[:message][:headers])
+        message_payload = @mandrill_delivery_handler.send(:get_message_payload, message)
+
+        assert_equal true, message_payload[:track_opens]
+        assert_equal true, message_payload[:track_clicks]
+        assert_equal 'Subject', message_payload[:message][:subject]
+        assert_equal 'foo@bar.com', message_payload[:message][:from_email]
+        assert_equal 'Mandrill Email Delivery Handler', message_payload[:message][:from_name]
+        assert_equal 'foo@bar.com has moved use awesome@awesomesauce.com now', message_payload[:message][:html]
+        assert_equal [{email: 'foo@bar.com', name: 'foo@bar.com'}], message_payload[:message][:to]
+        assert_equal({'Reply-To' => 'replyto@example.com'}, message_payload[:message][:headers])
       end
     end
   end
